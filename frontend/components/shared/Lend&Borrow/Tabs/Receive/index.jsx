888a7bf9f6a872fe25fa373fa6dceabf71c57fc0
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-11-30 03:24:09 +0100

    Receive section

--- frontend/components/shared/Lend&Borrow/Tabs/Receive/index.jsx
+++ frontend/components/shared/Lend&Borrow/Tabs/Receive/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
+import RowCard from '../../RowCard';
 import { EmptyState } from "../../EmptyState";
 
 const Receive = () => {
@@ -10,9 +10,59 @@ const Receive = () => {
 
   return (
     <div className="flex flex-col w-full justify-center items-center gap-6">
-     <EmptyState type="receive" />
+    <div className="bg-white rounded-lg shadow-sm">
+      <div className="grid grid-cols-3 py-3 px-6 bg-gray-50 border-b border-gray-200 text-sm font-medium text-gray-500">
+        <div>Sender</div>
+        <div>Interest Rate</div>
+        <div></div>
+      </div>
+      
+      {lendingData.map((item, index) => (
+        <RowCard key={index} {...item} />
+      ))}
+    </div>
+     {/* <EmptyState type="receive" /> */}
     </div>
   );
 };
 
 export default Receive;
+
+
+const lendingData = [
+  {
+    sender: 'HabbyFx Xchange',
+    isVerified: true,
+    availableSUI: 100,
+    minSUI: 400,
+    interestRate: '15UI'
+  },
+  {
+    sender: 'HabbyFx Xchange',
+    isVerified: false,
+    availableSUI: 100,
+    minSUI: 400,
+    interestRate: '15UI'
+  },
+  {
+    sender: 'HabbyFx Xchange',
+    isVerified: true,
+    availableSUI: 100,
+    minSUI: 400,
+    interestRate: '15UI'
+  },
+  {
+    sender: 'HabbyFx Xchange',
+    isVerified: false,
+    availableSUI: 100,
+    minSUI: 400,
+    interestRate: '15UI'
+  },
+  {
+    sender: 'HabbyFx Xchange',
+    isVerified: false,
+    availableSUI: 100,
+    minSUI: 400,
+    interestRate: '15UI'
+  }
+];
